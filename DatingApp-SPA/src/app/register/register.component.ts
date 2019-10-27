197cdd76600e80bdc994710997397b44cb78d155
David Rainey <david.rainey83@gmail.com>
2019-10-26 22:17:26 -0500

    Add reactive form controls to register component

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { BsDatepickerConfig } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-register',
@@ -10,17 +12,52 @@ import { AlertifyService } from '../_services/alertify.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  registerForm: FormGroup;
+  bsConfig: Partial<BsDatepickerConfig>;
+
+  constructor(private authService: AuthService,
+              private alertify: AlertifyService,
+              private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.bsConfig = {
+      containerClass: 'theme-red',
+      minDate: new Date(1900, 0, 1),
+      maxDate: new Date()
+    };
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group(
+      {
+        gender: ['male'],
+        username: ['', Validators.required],
+        knownAs: ['', Validators.required],
+        dateOfBirth: [null, Validators.required],
+        city: ['', Validators.required],
+        country: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+        confirmPassword: ['', Validators.required]
+      },
+      {
+        validators: [this.passwordMatchValidator]
+      }
+    );
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : { mismatch: true };
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
-      this.alertify.success('registration successful');
-    }, error => {
-      this.alertify.error(error);
-    });
+    // TODO: commented out for testing
+    // this.authService.register(this.model).subscribe(() => {
+    //   this.alertify.success('registration successful');
+    // }, error => {
+    //   this.alertify.error(error);
+    // });
+    console.log(this.registerForm.value);
   }
 
   cancel() {
