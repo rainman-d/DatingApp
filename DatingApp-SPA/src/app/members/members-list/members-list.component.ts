2e4401e494dce0e9b6a69dfd2b794794ee5fa81e
David Rainey <david.rainey83@gmail.com>
2020-01-08 20:33:34 -0600

    Add paging and sorting to web app

--- DatingApp-SPA/src/app/members/members-list/members-list.component.ts
+++ DatingApp-SPA/src/app/members/members-list/members-list.component.ts
@@ -3,6 +3,7 @@ import { User } from '../../_models/user';
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResult } from 'src/app/_models/pagination';
 
 @Component({
   selector: 'app-members-list',
@@ -11,12 +12,54 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MembersListComponent implements OnInit {
   users: User[];
+  user: User = JSON.parse(localStorage.getItem('user'));
+  genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}];
+  userParams: any = {};
+  pagination: Pagination;
+  lastSort: string;
 
   constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.users = data.users;
+      this.users = data.users.result;
+      this.pagination = data.users.pagination;
     });
+
+    this.resetFilters();
+  }
+
+  // event listener for pagination, when button clicked for different page this is fired
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+
+  resetFilters() {
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.userParams.orderBy = 'lastActive';
+    this.loadUsers();
+  }
+
+  // prevent unnecessary calls to API if same button clicked over and over
+  applySort() {
+    if (this.userParams.orderBy === this.lastSort) {
+      return;
+    }
+    this.loadUsers();
+  }
+
+  // loads users for pagination
+  loadUsers() {
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, this.userParams)
+      .subscribe((res: PaginatedResult<User[]>) => {
+        this.users = res.result;
+        this.pagination = res.pagination;
+        this.lastSort = this.userParams.orderBy;
+      }, error => {
+        this.alertify.error(error);
+      });
   }
 }
